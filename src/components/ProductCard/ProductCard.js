a4320486349bd6162fca6641dbba8155e28c04aa
7arj <arjunojha2004@gmail.com>
2024-06-24 11:02:18 +0530

    redux

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -2,13 +2,14 @@ import './ProductCard.css';
 import AddToCart from '../AddToCart/AddToCart';
 import { useRef, useState } from 'react';
 import logo1 from '../../assests/logo1.png';
-function ProductCard({ title, price }) {
+function ProductCard({ product }) {
   var a = 10;
   a = a + 1;
   let pRef = useRef(0);
   let iRef = useRef(0);
   let oRef = useRef(0);
   let [inputV, setInputV] = useState('Class');
+  console.log("pc", product.id)
 
   // setInput(value);
   function printTitle() {
@@ -31,12 +32,12 @@ function ProductCard({ title, price }) {
 
   return (
     <div className="product-card">
-      <p onClick={printTitle}> {title}</p>
-      <p ref={pRef}> {price.value}</p>
+      <p onClick={printTitle}> {product.title}</p>
+      <p ref={pRef}> {product.price.value}</p>
       <img src={logo1} />
       <input type="text" onChange={displayOutput} ref={iRef} value={inputV} />
       <p ref={oRef}>Over here the output would arrive - {inputV}</p>
-      <AddToCart />
+      <AddToCart product={product} />
     </div>
     )
   }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,5 +1,6 @@
 import ProductCard from "../ProductCard/ProductCard";
 import Effect from "../Effect/Effect";
+import React from "react";
 import { useState, useEffect } from "react";
 import useWindowSize from "../../hooks/useWindowSize";
 function Products() {
@@ -38,10 +39,10 @@ function Products() {
   // let products = [];
 
   let [products, setProducts] = useState([]);
-
+  console.log("products")
   console.log(useWindowSize());
   useEffect(() => {
-    fetch("https://run.mocky.io/v3/279c028e-f492-4e0f-932b-51dbed1b1dce").then(
+    fetch("https://run.mocky.io/v3/0912a49d-ab8c-4aa2-9363-d1d21fd3f66a").then(
       (response) => {
         return response.json();
       }
@@ -57,7 +58,7 @@ function Products() {
           <div><Effect /></div>
             {
                 products.map(function (item, index) {
-                  return (<ProductCard key={index}  title={item.title} price={item.price} />)
+                  return (<ProductCard key={index}  product={item} />)
                 })
             }
         </div>
@@ -65,7 +66,7 @@ function Products() {
 }
 export let a = 10;
 export let b = 20;
-export default Products;
+export default React.memo(Products);
 // Javascript XML 
 
 //ProductCard(title)
